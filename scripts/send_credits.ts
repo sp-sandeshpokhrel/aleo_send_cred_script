b21e9da81beee6aea5cf60e8c65f1944fbd9ed3c
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2025-02-27 13:49:18 +0545

    readme updated

--- scripts/send_credits.ts
+++ scripts/send_credits.ts
@@ -6,17 +6,26 @@ async function main() {
     // write script to transfer credits to users
 
     const creditsContract = new CreditsContract({ mode: ExecutionMode.SnarkExecute })
-    const [sender, receiver] = await creditsContract.getAccounts()
+    const [sender] = await creditsContract.getAccounts()
     const sender_credits = await creditsContract.account(sender)
-    const receiver_credits = await creditsContract.account(receiver)
-    console.log(`Current credits: ${sender_credits} for sender and ${receiver_credits} for receiver`)
-    const list_of_users = [sender]
-    const amount = BigInt(100000); // 1 ALEO credits=1000000
+    console.log(`Current credits: ${sender_credits}`)
+
+
+
+    const list_of_users: string[] = [sender]  // List of aleo users address in string
+    const amount = BigInt(123456); // 1 ALEO credits=1000000
     for (let i = 0; i < list_of_users.length; i++) {
-        const tx = await creditsContract.transfer_public(list_of_users[i], amount)
-        await tx.wait()
+        try {
+            console.log(`Transferring ${amount} credits to ${list_of_users[i]} index ${i}`)
+            const tx = await creditsContract.transfer_public(list_of_users[i], amount)
+            await tx.wait()
+            console.log('Transfer successful for user:', list_of_users[i])
+        } catch (e) {
+            console.log('Transfer failed for user:', list_of_users[i], 'index ', i)
+        }
     }
     const sender_credits_after = await creditsContract.account(sender)
-    const receiver_credits_after = await creditsContract.account(receiver)
-    console.log(`Credits after transfer: ${sender_credits_after} for sender and ${receiver_credits_after} for receiver`)
+    console.log(`Credits after transfer: ${sender_credits_after}`)
 }
+
+main()
